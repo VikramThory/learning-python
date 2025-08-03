69c8be7a9573502d61e3c86bac9ed36ad25a488b
Vikram Singh <vikramthory@gmail.com>
2025-08-03 15:42:04 +0530

    adding basic promgrams for input, print and datatypes.

---
+++ datatype.py
@@ -0,0 +1,19 @@
+amount = 100
+tax = 0.06
+total = amount + amount * tax
+print(total)
+
+int_total = int(total)
+print(int_total)
+
+float_total = float(total)
+print(float_total)
+
+name = 'Vikram'
+print(name)
+name = "Vikram Singh"
+print(name)
+
+name = input('What is your name?')
+greeting = 'Hello ' + name
+print(greeting)
