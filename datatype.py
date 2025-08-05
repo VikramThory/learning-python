de21c00474ef2c86aad94875708631383cb6a08e
Vikram Singh <vikramthory@gmail.com>
2025-08-05 22:37:10 +0530

    data types and different number forms.

--- datatype.py
+++ datatype.py
@@ -17,3 +17,26 @@ print(name)
 name = input('What is your name?')
 greeting = 'Hello ' + name
 print(greeting)
+
+a = 1
+b = 1.0
+print(type(a))
+print(type(b))
+
+c = 'String'
+print(type(c))
+
+d = 3 + 4j
+print(type(d))
+
+e = True # False
+print(type(e))
+
+f = 0B1010 # Binary
+print(type(f))
+
+g = 0XAF23 #Hexadecimal
+print(type(g))
+
+h = 0O234 #Octal
+print(type(h))
