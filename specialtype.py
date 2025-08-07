fb999fcf36e0fb21339ee915e718ddfc4ac93e5c
Vikram Thory <vikramthory1996@gmail.com>
2025-08-08 00:08:31 +0530

    special data type

---
+++ specialtype.py
@@ -0,0 +1,4 @@
+a = None # Means no value
+b = "Hello \n World!" # Using escape character
+PI = 3.14 # No special keyword for constant just variable name in all capital
+del a # deleting a variable and freeing the memory different from None can be used for List and Dic.
